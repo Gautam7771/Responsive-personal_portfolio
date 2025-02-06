e337d189641fe960fd8da7c386a19ea91d921599
Gautam7771 <gautamjadhav7771@gmail.com>
2025-02-06 18:04:30 +0530

    first commit

--- myproject/script.js
+++ myproject/script.js
@@ -45,9 +45,9 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['CSE Student at NMIT ','Frontend developer'],
+    strings:['Shopify Devloper ','Frontend developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
-});
+});.0
